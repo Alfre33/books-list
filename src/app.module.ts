1593f280a3657d8b1605cbefd02ee37d3dd0f07a
Alfredo Gaspar <josegaspr28383@gmail.com>
2025-11-25 17:54:02 -0600

    feat: initialize NestJS application, configure GraphQL with ApolloServer and MongoDB integration

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost/nest',
+      {
+        dbName: 'books-list',
+      },
+    ),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
